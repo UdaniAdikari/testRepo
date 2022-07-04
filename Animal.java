57562895ea0dd29342b9808fa57d487203e6541b
Udani_Adikari <97080392+UdaniAdikari@users.noreply.github.com>
2022-07-04 17:40:19 +0530

    Add files via upload

---
+++ Animal.java
@@ -0,0 +1,15 @@
+class RealWorld {
+    public static void main(String []args) {
+        System.out.println("This is the Java World");
+    }
+}
+public class Animal {
+    String name = "Zeebra";
+    public String getName() {
+    return name;
+    }
+}
+class Zoo {
+    //Zoo related info
+}
+    
---
+++ Zoo.java
@@ -0,0 +1,15 @@
+public class RealWorld {
+    public static void main(String []args) {
+        System.out.println("This is the Java World");
+    }
+}
+public class Animal {
+    String name = "Zeebra";
+    public String getName() {
+    return name;
+    }
+}
+public class Zoo {
+    //Zoo related info
+}
+    
